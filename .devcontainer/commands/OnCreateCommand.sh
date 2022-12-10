1d3c79996d78e7cfa67483cc7fef6a2cb90aeb35
Jorge Carballo <jorge@molmo.es>
2022-12-10 12:50:53 +0100

    feat(composer): Define volumes for cache and home folders

--- .devcontainer/commands/OnCreateCommand.sh
+++ .devcontainer/commands/OnCreateCommand.sh
@@ -1,10 +1,22 @@
+// Add Composer cache folder
+[[ ! -d $COMPOSER_HOME ]] && mkdir -p $COMPOSER_HOME
+[[ ! -d $COMPOSER_CACHE_DIR ]] && mkdir -p $COMPOSER_CACHE_DIR
+
+// Install Laravel
 if [[ ! -f "./composer.json" ]]
 then
     mv .git /tmp
     mv .github /tmp
     mv .devcontainer /tmp
 
-    docker run --rm --interactive --tty --volume ${PWD}:/app --user $(id -u):$(id -g) composer create-project laravel/laravel .
+    docker run --rm --interactive --tty \
+        --env COMPOSER_HOME \
+        --env COMPOSER_CACHE_DIR \
+        --volume ${PWD}:/app \
+        --volume ${COMPOSER_HOME}:$COMPOSER_HOME \
+        --volume ${COMPOSER_CACHE_DIR}:$COMPOSER_CACHE_DIR \
+        --user $(id -u):$(id -g) \
+        composer create-project laravel/laravel .
 
     mv /tmp/.git .
     mv /tmp/.github .
