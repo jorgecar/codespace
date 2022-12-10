4b505e466cdfe7d21ac654a2dfd9fcfc212bf619
Jorge Carballo <jorge@molmo.es>
2022-12-10 12:13:41 +0100

    refactor(devcontainer): Remove whitespaces

--- .devcontainer/commands/OnCreateCommand.sh
+++ .devcontainer/commands/OnCreateCommand.sh
@@ -3,7 +3,7 @@ then
     mv .git /tmp
     mv .github /tmp
     mv .devcontainer /tmp
-    
+
     docker run --rm --interactive --tty --volume ${PWD}:/app --user $(id -u):$(id -g) composer create-project laravel/laravel .
 
     mv /tmp/.git .
